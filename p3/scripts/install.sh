0d1ca799247419726dcca00b0d99c922e20556ee
Carson JOZEFZOON <cjozefzo@f1r6s4.clusters.42paris.fr>
2025-12-15 15:28:41 +0100

    test

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -18,7 +18,7 @@ yes | sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugi
 
 #install k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-k3d cluster create my-cluster --api-port 6443 -p "8080:80@loadbalancer"
+k3d cluster create my-cluster --api-port 6443 -p "192.168.56.110:8080:80@loadbalancer"
 
 #install kubectl
 sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -32,11 +32,16 @@ sudo kubectl create namespace dev
 sudo usermod -aG docker vagrant
 
 #add ssh key to authorizedkey
-cat /home/vagrant/.ssh/my_key.pub >> /home/vagrant/.ssh/authorized_keys
+#cat /home/vagrant/.ssh/my_key.pub >> /home/vagrant/.ssh/authorized_keys
 #instaling argocd cli
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
+HOME=/home/vagrant
+sudo kubectl patch deployment argocd-server -n argocd \
+  --type='json' \
+  -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args", "value": ["--insecure"]}]'
+sudo kubectl apply -f $HOME/ingressroute.yaml
 #kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d //get password to connect on browser
 #kubectl port-forward svc/argocd-server -n argocd 8080:443 //port-forward to access from browser
 #argocd login localhost:8080 //login to argocd
